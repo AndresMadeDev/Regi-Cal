2903562673f6c744b8422873dba9d65b57778c1f
AndresMadeDev <andres.made.dev@gmail.com>
2023-10-02 22:00:58 -0400

    Initial Commit

---
+++ RegisterCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RegisterCalculator
+//
+//  Created by Andres Made on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RegisterCalculator/RegisterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterCalculatorApp.swift
+//  RegisterCalculator
+//
+//  Created by Andres Made on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RegisterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
